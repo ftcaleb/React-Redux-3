0c18fe6455b5d73f4db377be443473f3bfeb4fe1
ftcaleb <caleb19scott@gmail.com>
2025-11-12 18:10:48 +0200

    update

---
+++ React-redux-project/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store'; // Assuming this is your Redux store
+import App from './App'; // Your main App component
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
